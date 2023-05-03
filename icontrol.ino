6ef2817efae67deeb44f82473668c7e537d6d058
lfer9980 <luis_fernandez99@hotmail.com>
2023-05-02 21:04:02 -0600

    :art: cambios para el ECG

--- icontrol.ino
+++ icontrol.ino
@@ -4,110 +4,127 @@
 #include <ArduinoJson.h>
 #include <ArduinoHttpClient.h>
 
-//config el ethernet
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; 
+// config el ethernet
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168, 0, 10);
 char url[] = "192.168.0.6";
 
-//config DHT
-#define DHTPIN 5 
+// config DHT
+#define DHTPIN 5
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-//config http
+// config http
 EthernetClient ether;
-HttpClient client = HttpClient(ether,url,80);
+HttpClient client = HttpClient(ether, url, 80);
 
-//config json body
+// config json body
 StaticJsonDocument<64> doc;
 
-//variables
+// variables
 bool posting = false;
 
-void setup(){
-   
-   Serial.begin(9600);
+void setup()
+{
 
-   pinMode(3, OUTPUT);
-   pinMode(4, OUTPUT);
-   
-   dht.begin();
-   Serial.println("Initialize Ethernet with DHCP:");
-    if (Ethernet.begin(mac) == 0) {
-      Serial.println("Failed to configure Ethernet using DHCP");
-      if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-        Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-        while (true) {
-          delay(1); // do nothing, no point running without Ethernet hardware
-        }
+  Serial.begin(9600);
+
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  dht.begin();
+  Serial.println("Initialize Ethernet with DHCP:");
+  if (Ethernet.begin(mac) == 0)
+  {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    if (Ethernet.hardwareStatus() == EthernetNoHardware)
+    {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true)
+      {
+        delay(1); // do nothing, no point running without Ethernet hardware
       }
-      if (Ethernet.linkStatus() == LinkOFF) {
-        Serial.println("Ethernet cable is not connected.");
-      }
-      Ethernet.begin(mac, ip);
     }
-    Serial.println("Connecting.....");
-    delay(1000);
-  
-    IPAddress ip = Ethernet.localIP();
-    Serial.print("IP Address: ");
-    Serial.println(ip);
-    Serial.println();
+    if (Ethernet.linkStatus() == LinkOFF)
+    {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    Ethernet.begin(mac, ip);
+  }
+  Serial.println("Connecting.....");
+  delay(1000);
+
+  IPAddress ip = Ethernet.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println();
 }
 
-void leds(int hum, int temp){
-  if (hum == 1 and temp == 1){
-    digitalWrite(3 ,HIGH);
-    digitalWrite(4 ,HIGH);
-  }else if (hum == 1 and temp == 0){
-    digitalWrite(3 ,HIGH);
-    digitalWrite(4 ,LOW);
-  }else if (hum == 0 and temp == 1){
-    digitalWrite(3 ,LOW);
-    digitalWrite(4 ,HIGH);
-  }else{
-    digitalWrite(3 ,LOW);
-    digitalWrite(4 ,LOW);
+void leds(int hum, int temp)
+{
+  if (hum == 1 and temp == 1)
+  {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  else if (hum == 1 and temp == 0)
+  {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  }
+  else if (hum == 0 and temp == 1)
+  {
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+  }
+  else
+  {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
   }
 }
 
-void get(){
+void get()
+{
 
   Serial.println("=========GET=========");
-  
+
   client.beginRequest();
   client.get("/api/v1/public/getdatos");
   client.sendHeader("Host: ", url);
   client.println();
   client.endRequest();
-  
+
   int statusCode = client.responseStatusCode();
   String response = client.responseBody();
 
-  if (statusCode == 200){
+  if (statusCode == 200)
+  {
     DeserializationError error = deserializeJson(doc, response);
 
     bool ishot = doc["ishot"]; // true
     bool ishum = doc["ishum"]; // false
-    
+
     Serial.println(response);
 
     leds(ishum, ishot);
-  }else{
+  }
+  else
+  {
 
     Serial.println("Ocurrio un error");
   }
 
   delay(2000);
-  
 }
 
-void post(String data){
+void post(String data)
+{
 
   Serial.println("=========POST=========");
 
   Serial.println(data);
-  
+
   client.beginRequest();
   client.post("/api/v1/public/datos");
   client.sendHeader("Host: ", url);
@@ -117,36 +134,39 @@ void post(String data){
   client.print(data);
   client.endRequest();
 
-
   int statusCode = client.responseStatusCode();
   String response = client.responseBody();
 
-  if (statusCode == 200){
+  if (statusCode == 200)
+  {
     DeserializationError error = deserializeJson(doc, response);
     Serial.println(response);
-    
-  }else{
+  }
+  else
+  {
 
     Serial.println("Ocurrio un error");
   }
 
   delay(2000);
-  
 }
 
-void loop() {
+void loop()
+{
 
-  if (posting == true){
+  if (posting == true)
+  {
     float hum = dht.readHumidity();
     float temp = dht.readTemperature();
     String data = "temp=" + String(temp) + "&hum=" + String(hum);
     post(data);
     posting = false;
-  }else{
+  }
+  else
+  {
     get();
     posting = true;
   }
 
   delay(1000);
-  
 }
